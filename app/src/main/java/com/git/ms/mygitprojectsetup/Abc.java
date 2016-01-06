96f46dbc85059451b227565f4fd79b13099f4479
praveen <praveenkumar046@gmail.com>
2016-01-06 15:48:35 +0530

    Commit Second Message

---
+++ app/src/main/java/com/git/ms/mygitprojectsetup/Abc.java
@@ -0,0 +1,12 @@
+package com.git.ms.mygitprojectsetup;
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Abc extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
